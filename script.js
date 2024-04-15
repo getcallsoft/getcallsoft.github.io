95e7e12e17a809de1d1508660414cb081a2b08c1
Richard Z <z-zrichard@hotmail.com>
2024-04-14 22:28:43 -0400

    updated design

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('signupForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var email = document.getElementById('email').value;
-    // Normally, you would send this data to a server. Example:
-    // window.location.href = `https://formspree.io/f/{your_id}?email=${encodeURIComponent(email)}`;
-    alert('Thank you for signing up, ' + email);
-});
